b7f0d8fa26aef7e7b1bed18108654f910beb05a3
liwBh <liwbarqueroh@gmail.com>
2024-07-20 12:40:30 -0600

    agregando botón de ver password

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,14 +1,30 @@
-
 document.addEventListener('DOMContentLoaded', function () {
 
-    let navbarHeight = document.getElementById('navbar').offsetHeight;
-    navbarHeight += 2;
-    console.log(navbarHeight)
-    const sidebar = document.getElementById('sidebar');
-    sidebar.style.minHeight = `calc(100vh - ${navbarHeight}px)`;
+    // Sidebar height adjustment
+    if (document.getElementById('navbar') && document.getElementById('sidebar')) {
+        let navbarHeight = document.getElementById('navbar').offsetHeight;
+        navbarHeight += 2;
+        //console.log(navbarHeight)
+        const sidebar = document.getElementById('sidebar');
+        sidebar.style.minHeight = `calc(100vh - ${navbarHeight}px)`;
 
-    console.log(sidebar)
+        //console.log(sidebar)
+    }
 
+    // Update current year dynamically in footer and form auth
+    if (document.getElementById('currentYear')) {
+        document.getElementById('currentYear').textContent = new Date().getFullYear();
+    }
+
+});
+
+
+$(document).ready(function () {
+    $('.show-password').click(function () {
+        const passwordField = $(this).siblings('.password-input');
+        const type = passwordField.attr('type') === 'password' ? 'text' : 'password';
+        passwordField.attr('type', type);
+    });
 });
 
 
